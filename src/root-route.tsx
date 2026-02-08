a0399d026a23cf3395e90de92e63b4447841f7cd
heejaykong <heejay.dev@gmail.com>
2026-02-08 18:56:44 +0900

    feat: 라우터 가드 구현

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -6,20 +6,26 @@ import IndexPage from "@/pages/index-page";
 import PostDetailPage from "@/pages/post-detail-page";
 import ProfileDetailPage from "@/pages/profile-detail-page";
 import ResetPasswordPage from "@/pages/reset-password-page";
-import GlobalLayout from "./components/layout/global-layout";
+import GlobalLayout from "@/components/layout/global-layout";
+import GuestsOnlyLayout from "@/components/layout/guests-only-layout";
+import MembersOnlyLayout from "@/components/layout/members-only-layout";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route element={<GuestsOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route element={<MembersOnlyLayout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
